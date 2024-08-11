e21a5e801a67e819e9d2d6f259f12681f37526d9
DanutPopa <popa.ion.danut@gmail.com>
2024-08-11 10:59:41 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Danut Popa on 11.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
